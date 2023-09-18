9d6d4df863d33a027e99be0592bfd339750e82d5
Yasharbek S <118141779+yasharbek@users.noreply.github.com>
2023-09-18 01:20:16 -0400

    Update adjacencyListGraph-inl.h

--- adjacencyListGraph-inl.h
+++ adjacencyListGraph-inl.h
@@ -1,11 +1,3 @@
-/*
-  Copyright (c) 2019
-  Swarthmore College Computer Science Department, Swarthmore PA
-  J. Brody, A. Danner, M. Gagne, L. Meeden, Z. Palmer, A. Soni, M. Wehar
-  Distributed as course material for Fall 2019
-  CPSC 035: Data Structures and Algorithms
-*/
-
 #pragma once
 
 #include "stringUtils.h"
