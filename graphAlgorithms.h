056d57ad7f23983a9f46fa8501d91deef5c57693
Yasharbek S <118141779+yasharbek@users.noreply.github.com>
2023-09-18 01:22:20 -0400

    Update graphAlgorithms.h

--- graphAlgorithms.h
+++ graphAlgorithms.h
@@ -1,11 +1,3 @@
-/*
-  Copyright (c) 2021
-  Swarthmore College Computer Science Department, Swarthmore PA
-  J. Brody, S. Caplan, A. Danner, L. Meeden, Z. Palmer, A. Soni, M. Wehar
-  Distributed as course material for Fall 2021
-  CPSC 035: Data Structures and Algorithms
-*/
-
 #pragma once
 
 #include <vector>
