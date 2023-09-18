3c766cb6c6400edc01c10602bef09719d20a85f5
Yasharbek S <118141779+yasharbek@users.noreply.github.com>
2023-09-18 01:24:19 -0400

    Update stringUtils.h

--- stringUtils.h
+++ stringUtils.h
@@ -1,13 +1,5 @@
 #pragma once
 
-/*
-  Copyright (c) 2021
-  Swarthmore College Computer Science Department, Swarthmore PA
-  J. Brody, S. Caplan, A. Danner, L. Meeden, Z. Palmer, A. Soni, M. Wehar
-  Distributed as course material for Fall 2021
-  CPSC 035: Data Structures and Algorithms
-*/
-
 // This module contains a helper function to standardize error message
 // generation in template classes.
 
